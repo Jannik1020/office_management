d8036e901398782d17aa57e41d2244187d0fc372
Jannik1020 <jannik.thiele.official@gmail.com>
2021-05-11 14:10:29 +0200

    handled warnings

--- src/app/App.js
+++ src/app/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useState } from "react";
+import React from "react";
 
 import Navigation from "../features/navigation/Navigation";
 import TaskManagement from "../features/tasks/TaskManagement";
